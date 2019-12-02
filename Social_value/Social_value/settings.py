bb1037979d508136fb05da34699aeb8a346b8896
CatherineMackay <catmackay1106@gmail.com>
2019-12-02 15:51:01 +0000

    settings.py app name correction

--- Social_value/Social_value/settings.py
+++ Social_value/Social_value/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Social-value-app',
+    'Social_value_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
