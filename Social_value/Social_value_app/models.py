543886d0e8fd94ead07f4f56fa692c1c5d3cb34d
CatherineMackay <catmackay1106@gmail.com>
2019-12-02 16:59:58 +0000

    Corrected errors

--- Social_value/Social_value_app/models.py
+++ Social_value/Social_value_app/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 import datetime
 
-RAG_CHOICES = (
+#to be changed later
+"""RAG_CHOICES = (
     (1, 'Green')
     (2, 'Amber')
     (3, 'Red')
     (4, 'N/A')
-)
+)"""
 
 class Question(models.Model):
     question_no = models.IntegerField(primary_key=True)
@@ -14,7 +15,7 @@ class Question(models.Model):
     eval_tx = models.CharField(max_length=1000, default="")
     evidence_tx = models.CharField(max_length=1000, default="")
     results_tx = models.CharField(max_length=1000, default="")
-    rag_conditional_int = models.IntegerField(choices=RAG_CHOICES, default=4)
+    rag_conditional_int = models.IntegerField(default=4)
 
 class Quadrants(models.Model):
     quadrant_no = models.IntegerField(primary_key=True) 
@@ -25,10 +26,10 @@ class Questionnaire(models.Model):
     questionnaire_type_no = models.IntegerField(primary_key=True)
     questionnaire_type_tx = models.CharField(max_length=100, default="")
     completed_date_dt = models.DateField(default=datetime.date.today())
-    quadrant_no = models.ForeignKey(Quadrants, on_delete=models.cascade)
+    quadrant_no = models.ForeignKey(Quadrants, on_delete=models.CASCADE)
 
 class SME(models.Model):
     company_id_no = models.IntegerField(primary_key=True)
-    SME_name_tx = models.CharField(max_length=3000, defaults="")
+    SME_name_tx = models.CharField(max_length=3000, default="")
     email_tx = models.CharField(max_length=3000, default="")
     questionnaire_no = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
